dfacc76ae0202592331073b0164914ed3e91b77a
Eatmeta <metagroul@gmail.com>
2022-10-28 22:35:36 +0300

    Implement a method Solve that returns number of "Lucky Tickets" with a given sum of digits, among those whose number consists of 2N digits

--- TicketsTask.cs
+++ TicketsTask.cs
@@ -1,10 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Numerics;
 
 namespace Tickets
 {
-    
+    public static class TicketsTask
+    {
+        public static BigInteger Solve(int halfLen, int totalSum)
+        {
+            if (totalSum % 2 != 0)
+                return 0;
+
+            totalSum /= 2;
+            BigInteger result = default;
+            var opt = new BigInteger[totalSum + 1, halfLen];
+
+            for (var i = 0; i <= Math.Min(totalSum, 9); ++i)
+                opt[i, 0] = 1;
+            
+            for (var i = 0; i < halfLen; ++i)
+                opt[0, i] = 1;
+
+            for (var i = 1; i <= totalSum; i++)
+            for (var j = 1; j < halfLen; j++)
+            for (var k = 0; k <= Math.Min(i, 9); k++)
+                opt[i, j] += opt[i - k, j - 1];
+            
+            result = opt[totalSum, halfLen - 1];
+            
+            return result * result;
+        }
+    }
 }
--- TicketsTask_should.cs
+++ TicketsTask_should.cs
@@ -17,8 +17,20 @@ namespace Tickets
         [Test]
         public void Test2() { MakeTest(2, 5, "0"); }
         [Test]
+        public void Test3() { MakeTest(1, 18, "1"); }
+        [Test]
+        public void Test4() { MakeTest(2, 20, "81"); }
+        [Test]
+        public void Test5() { MakeTest(3, 54, "1"); }
+        [Test]
         public void Test6() { MakeTest(10, 10, "4008004"); }
         [Test]
         public void Test7() { MakeTest(20, 20, "401200499400100"); }
+        [Test]
+        public void Test8() { MakeTest(3, 52, "9"); }
+        [Test]
+        public void Test9() { MakeTest(50, 50, "1228576490354119571810717714807944388004"); }
+        [Test]
+        public void Test10() { MakeTest(1, 11, "0"); }
     }
 }
